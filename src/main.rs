3ac335ad1ea5163b9ef64715a0d250704737ff83
Garry O'Donnell <garry.euan.odonnell@ntlworld.com>
2024-10-18 11:26:43 +0100

    Setup tracing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,23 @@
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
 
+use clap::Parser;
+use tracing::Level;
+
+/// Tristimg performs binning of event mode data, generating images
+#[derive(Debug, Parser)]
+struct Cli {
+    /// The minimum log level which should be produced
+    #[clap(long, env = "LOG_LEVEL", default_value_t=Level::INFO)]
+    log_level: Level,
+}
+
 fn main() {
-    println!("Hello, world!");
+    _ = dotenvy::dotenv();
+    let args = Cli::parse();
+    tracing_subscriber::fmt()
+        .with_max_level(args.log_level)
+        .init();
+
+    tracing::info!("Hello, world!");
 }
